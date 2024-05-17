87f53ba6a4f322e784aab7b9c4cf293a53322231
KimYong91 <rladyd97@naver.com>
2024-05-17 15:44:04 +0900

    액션바

---
+++ ch07_appbar/src/main/java/com/busanit/ch07_appbar/ToolBarActivity.kt
@@ -0,0 +1,35 @@
+package com.busanit.ch07_appbar
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.busanit.ch07_appbar.databinding.ActivityToolBarBinding
+
+class ToolBarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityToolBarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbar.run {
+            title = "툴 바"
+            setTitleTextColor(Color.WHITE)
+            setOnMenuItemClickListener{
+                when (it.itemId) {
+                    R.id.item1 -> {
+                        Toast.makeText(this@ToolBarActivity, "항목 1을 눌렀습니다.", Toast.LENGTH_LONG).show()
+                        false
+                    }
+                    R.id.item2 -> {
+                        Toast.makeText(this@ToolBarActivity, "항목 2를 눌렀습니다.", Toast.LENGTH_LONG).show()
+                        false
+                    } else -> false
+
+
+                }
+            }
+        }
+
+    }
+}
