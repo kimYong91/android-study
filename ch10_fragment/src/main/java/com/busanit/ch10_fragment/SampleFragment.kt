0d1de1295dbc1a60ea3bb14ccc2c90c152cc950d
KimYong91 <rladyd97@naver.com>
2024-05-23 14:03:30 +0900

    프레그먼트 생성, 교체, 삭제

---
+++ ch10_fragment/src/main/java/com/busanit/ch10_fragment/SampleFragment.kt
@@ -0,0 +1,22 @@
+package com.busanit.ch10_fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class SampleFragment : Fragment() {
+
+    // Fragement의 뷰를 생성하고 반환하는 메서드 (생명주기)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // fragment_sample.XML 레이아웃을 뷰 객체로 반환
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sample, container, false)
+    }
+
+}
---
+++ ch10_fragment/src/main/java/com/busanit/ch10_fragment/manager/Frag1Activity.kt
@@ -0,0 +1,55 @@
+package com.busanit.ch10_fragment.manager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.busanit.ch10_fragment.R
+import com.busanit.ch10_fragment.databinding.ActivityFrag1Binding
+
+class Frag1Activity : AppCompatActivity() {
+    lateinit var binding: ActivityFrag1Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityFrag1Binding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        // 프레그먼트 추가
+        binding.addButton.setOnClickListener {
+
+            // 관리자를 통해 트랜잭션 시작
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.run {
+                add(R.id.fragmentContainerView, FirstFragment()) // 트랜잭션 추가
+                addToBackStack(null) // 트랜잭션을 백스택에 추가
+                commit()        // 트랜잭션 완료
+            }
+        }
+
+        // 프레그먼트 교체
+        binding.replaceButton.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+
+            transaction.run {
+                replace(R.id.fragmentContainerView, SecondFragment()) // 트랜잭션 교체
+                addToBackStack(null) // 트랜잭션을 백스택에 추가
+                commit()        // 트랜잭션 완료
+            }
+        }
+
+        // 프레그먼트 삭제
+        binding.removeButton.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+
+            // 컨테이너 리소스 ID를 기준으로 현제 사용하고 있는 프레그먼트를 찾는다.
+            val fragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+
+            transaction.run {
+                // 찾아낸 프레그먼트 제거
+                if (fragment != null) {
+                    remove(fragment)
+                }
+                commit()        // 트랜잭션 완료
+            }
+        }
+    }
+}
